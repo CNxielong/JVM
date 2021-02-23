fac6612113ad97bca9466997f2cc5b6b436d7b65
XieLong <cnxielong@gmail.com>
2021-02-23 21:11:28 +0800

    Update ReferenceQueueTest.java

--- com/icbc/jvm/gc/ReferenceQueueTest.java
+++ com/icbc/jvm/gc/ReferenceQueueTest.java
@@ -7,4 +7,8 @@ package com.icbc.jvm.gc;
  * @Date 2021/2/7/007 1:03
  */
 public class ReferenceQueueTest {
+    public static void main(String[] args) {
+        int a = 2^31;
+        System.out.println((Integer.MAX_VALUE+2)& 0x00ff);
+    }
 }
